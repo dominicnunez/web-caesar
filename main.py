2df761662a3e18c3d1781b28892a790a5a688372
Dominic <dominic.nunez@gmail.com>
2018-10-09 03:35:29 -0500

    proper setup of template directories, added favicon

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
-from flask import Flask
-import random, webbrowser
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-
-html_file = open("web-caesar.html", "r")
-html_list = html_file.readlines()
-html_file.close()
-form = """<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <link rel="stylesheet" href="style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Web Caesar</title>
-</head>
-
-<body>
-    <header>
-        <h1>Web Caesar</h1>
-    </header>
-    <main></main>
-    <footer></footer>
-</body>
-
-</html>"""
-
-@app.route("/")
-
-def form_html():
-    html = ""
-    for string in html_list:
-        html += string
-    return html
+@app.route('/')
 
 def index():
-    wc_html = form_html()
-    return wc_html
+   return render_template("web-caesar.html")
 
+@app.route('/favicon.ico')
 
-app.run()
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    app.run(debug = True)
