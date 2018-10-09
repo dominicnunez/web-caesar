49aaa6b0f4b06173a45eb9194668b2e2f5604859
Dominic <dominic.nunez@gmail.com>
2018-10-09 04:02:43 -0500

    changed html title

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-   return render_template("web-caesar.html")
 
 @app.route('/favicon.ico')
 
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+
+def index():
+   return render_template("web-caesar.html")
+
 
 if __name__ == '__main__':
     app.run(debug = True)
